1159dc6857a99d8369cd092e9efbfaec6e7d26b2
Ruan Victor <ruanvictormr@gmail.com>
2024-06-14 23:48:18 -0300

    persistencia no local storage

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-const colums = document.querySelectorAll(".column__cards");
-const cards = document.querySelectorAll(".card");
+const columns = document.querySelectorAll(".column__cards");
 
 let draggedCard;
+
+const saveToLocalStorage = () => {
+  const data = Array.from(columns).map((column) => {
+    return Array.from(column.querySelectorAll(".card")).map(
+      (card) => card.textContent
+    );
+  });
+  localStorage.setItem("cards", JSON.stringify(data));
+};
 
 const dragStart = (ev) => {
   draggedCard = ev.target;
@@ -11,6 +19,7 @@ const dragStart = (ev) => {
 const dragOver = (ev) => {
   ev.preventDefault();
 };
+
 const dragEnter = ({ target }) => {
   if (target.classList.contains("column__cards")) {
     target.classList.add("column--highlight");
@@ -25,10 +34,11 @@ const drop = ({ target }) => {
   if (target.classList.contains("column__cards")) {
     target.classList.remove("column--highlight");
     target.append(draggedCard);
+    saveToLocalStorage();
   }
 };
 
-const createCard = ({ target }) => {
+const createCard = (target, content = "", focus = true) => {
   if (!target.classList.contains("column__cards")) {
     return;
   }
@@ -38,29 +48,44 @@ const createCard = ({ target }) => {
   card.className = "card";
   card.draggable = true;
   card.contentEditable = true;
+  card.textContent = content;
 
   card.addEventListener("focusout", () => {
     card.contentEditable = false;
 
     if (!card.textContent) {
       card.remove();
+      saveToLocalStorage();
+    } else {
+      saveToLocalStorage();
     }
   });
 
   card.addEventListener("dragstart", dragStart);
 
   target.append(card);
-  card.focus();
+  if (focus) {
+    card.focus();
+  }
 };
 
-colums.forEach((column) => {
+columns.forEach((column) => {
   column.addEventListener("dragover", dragOver);
   column.addEventListener("dragenter", dragEnter);
   column.addEventListener("dragleave", dragLeave);
   column.addEventListener("drop", drop);
-  column.addEventListener("dblclick", createCard);
+  column.addEventListener("dblclick", ({ target }) => createCard(target));
 });
 
-cards.forEach((card) => {
-  card.addEventListener("dragstart", dragStart);
+window.addEventListener("load", () => {
+  let localJsons = localStorage.getItem("cards");
+  if (localJsons) {
+    localJsons = JSON.parse(localJsons);
+    localJsons.forEach((cards, index) => {
+      const column = columns[index];
+      cards.forEach((cardContent) => {
+        createCard(column, cardContent, false);
+      });
+    });
+  }
 });
